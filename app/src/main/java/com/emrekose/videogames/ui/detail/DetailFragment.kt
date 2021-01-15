359a876644e12184f55acecd3199d863875f565a
Emre Köse <e.kose26@gmail.com>
2021-01-15 17:23:20 +0300

    Navigation Component with BottomNavigationView implementation

---
+++ app/src/main/java/com/emrekose/videogames/ui/detail/DetailFragment.kt
@@ -0,0 +1,16 @@
+package com.emrekose.videogames.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.emrekose.videogames.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+}
---
+++ app/src/main/java/com/emrekose/videogames/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,16 @@
+package com.emrekose.videogames.ui.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.emrekose.videogames.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FavoritesFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+}
